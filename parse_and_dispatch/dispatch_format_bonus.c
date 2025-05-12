4820a320e709ee46320e722ff468156c8cd3bf6f
ymizuniw <newerdaysreki@gmail.com>
2025-05-12 18:44:41 +0900

    utoa_buf.c, utoa_base_buf.c_written

---
+++ parse_and_dispatch/dispatch_format_bonus.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   dispatch_format_bonus.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ymizuniw <ymizuniw@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/12 15:44:14 by ymizuniw          #+#    #+#             */
+/*   Updated: 2025/05/12 16:02:10 by ymizuniw         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	dispatch_format_bonus(const char *s, va_list args, t_buffer *buf)
+{
+	t_format	f;
+	int			i;
+
+	i = 0;
+	f = parse_format(s, &i, args);
+	if (f.specifier == 'c')
+		return (put_char_format(va_arg(args, int), f, buf) + i + 1);
+	if (f.specifier == 's')
+		return (put_str_format(va_arg(args, char *), f, buf) + i + 1);
+	if (f.specifier == 'd' || f.specifier == "i")
+		return (apply_format_int(va_arg(args, int), f, buf) + i + 1);
+	if (f.specifier == 'u')
+		return (apply_format_unsigned(va_arg(args, unsigned int), f, buf) + i + 1);
+	if (f.specifier == 'x' || f.specifier == 'X')
+		return (apply_format_hex(va_arg(args, unsigned int), f, buf,
+			f.specifier == 'X') + i + 1);
+	if (f.specifier == 'p')
+		return (apply_format_ptr(va_arg(args, void *), f, buf) + i + 1);
+	if (f.specifier == '%')
+		return (put_percent_format(f, buf) + i + 1);
+	return (0);
+}
