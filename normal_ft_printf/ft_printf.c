9d4a112fbe2d290ac0dd1bc09bfa2fd61484b1e6
Yugo Mizuniwa <ymizuniw@c2r9s3.42tokyo.jp>
2025-05-30 01:41:18 +0900

    logic sort

--- normal_ft_printf/ft_printf.c
+++ normal_ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ymizuniw <ymizuniw@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/29 03:31:37 by ymizuniw          #+#    #+#             */
-/*   Updated: 2025/05/29 03:57:32 by ymizuniw         ###   ########.fr       */
+/*   Updated: 2025/05/29 21:10:20 by ymizuniw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,27 +16,7 @@ int ft_printf(const char *format, ...)
 
 	//va_start
 	//parse_args
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			if (*format = 'c')
-				ft_putchar();
-			else if (*format = 's')
-				ft_putstr();
-			else if (*format = 'd' || *format = 'i')
-				ft_putint();
-			else if (*format = 'u')
-				ft_put_uint();
-			else if (*format = 'x' || *format = 'X')
-				ft_put_hex();
-			else if (*format = 'p')
-				ft_put_ptr();
-			else if (*format = '%')
-				ft_putchar('%');
-		}
-		format++;
-	}
+
 	//return (count)
 }
 
